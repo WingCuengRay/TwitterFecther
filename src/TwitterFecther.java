207392c6dadd14d7b0734617cee518c074740827
Raymand <yongconglei@gmail.com>
2018-03-05 23:28:17 -0500

    [Topic] update api key and serect

--- src/TwitterFecther.java
+++ src/TwitterFecther.java
@@ -14,10 +14,10 @@ public class TwitterFecther {
     public static void main(String[] args) throws Exception{
         ConfigurationBuilder cb = new ConfigurationBuilder();
         cb.setDebugEnabled(true)
-                .setOAuthConsumerKey("your key")
-                .setOAuthConsumerSecret("your secret")
-                .setOAuthAccessToken("your key")
-                .setOAuthAccessTokenSecret("your secret");
+                .setOAuthConsumerKey("WUZeAUiJyJFySY2I5C7oTkaRB")
+                .setOAuthConsumerSecret("QcmbvjQrSxLscxtZP6PndCYVEXxgBOoZ5g8ryvJLBYAmDTtrPx")
+                .setOAuthAccessToken("2965074672-HcndnMSZkdDKNqF1vqoERR1nynKLnKKqhMovkw4")
+                .setOAuthAccessTokenSecret("mue5UQ1QWSwGWDgf1lDTnrSeLJFVJLZltQxdyL34u0C0a");
 
         TwitterFactory t_facotry = new TwitterFactory(cb.build());
         Twitter twitter = t_facotry.getInstance();
